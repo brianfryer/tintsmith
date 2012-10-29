107c941204e0b1d5624ffe1324c33bfbb0b2b551
Brian Fryer <brianfryer@gmail.com>
2012-10-29 14:05:13 -0500

    Ignoring all admin themes except for 'default'.

--- app/app.rb
+++ app/app.rb
@@ -8,21 +8,6 @@ class Tintsmith < Padrino::Application
   enable :sessions
 
   ##
-  # Caching support
-  #
-  # register Padrino::Cache
-  # enable :caching
-  #
-  # You can customize caching store engines:
-  #
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Memcached.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Dalli::Client.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Redis.new(::Redis.new(:host => '127.0.0.1', :port => 6379, :db => 0))
-  #   set :cache, Padrino::Cache::Store::Memory.new(50)
-  #   set :cache, Padrino::Cache::Store::File.new(Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
-  #
-
-  ##
   # Application configuration options
   #
   # set :raise_errors, true       # Raise exceptions (will stop application) (default for test)
